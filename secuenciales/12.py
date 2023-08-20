5e9f03a3228d988a15000fc381968c107d147850
bryan <bryan123david@gmail.com>
2023-08-19 20:19:47 -0500

    py

---
+++ secuenciales/12.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+print("Ecuacion de segundo Grado:  ax2 + bx +c")
+
+a=int(input("Valor de a: "))
+x=int(input("Valor de x: "))
+b=int(input("Valor de b: "))
+c=int(input("Valor de c: "))
+
+resolucion=a*x**2+b*x+c
+
+print("resultado: ",resolucion)
---
+++ secuenciales/13.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+Copuesto=int(input("Cateto Opuesto: "))
+Cadyacente=int(input("Cateto Adyacente: "))
+
+Hipotenusa=(Copuesto**2+Cadyacente**2)**0.5
+
+print("Hipotenusa: ",Hipotenusa)
+
